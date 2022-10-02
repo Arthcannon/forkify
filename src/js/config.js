fb99cc1336a6d1f74602b3045bbd03bb98db2cee
Arthcannon <arthcannon@gmail.com>
2022-10-02 14:00:40 +0200

    Cleaning files

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6f0c78c3-4d29-4439-92f0-1293f686d3f3';
+export const MODAL_CLOSE_SEC = 2.5;
